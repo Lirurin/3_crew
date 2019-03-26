001ade28df25ecd46b64bb212d6d5f295675ea88
Lirurin <zerosouler@gmail.com>
2019-03-26 17:08:16 +0300

    Hidden menu complete, small changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
     var slides = document.querySelectorAll('#slides .slide');
     var controlEnb = document.querySelectorAll('#controls .control');
+    var toggler = document.querySelector('.home__toggle');
+    var navList = document.querySelector('.home__hidList');
+    var navigation = document.querySelector('navigation');
     var currentSlide = 0;
     var sliderDots = document.getElementsByClassName("control_button");
     var slideInterval = setInterval(nextSlide,8000);
@@ -9,7 +12,14 @@ document.addEventListener("DOMContentLoaded", function(){
         document.getElementsByClassName("cumf_bt_form_wrapper")[0].parentNode.removeChild(document.getElementsByClassName("cumf_bt_form_wrapper")[0]); 
         } catch(e) { 
         //catch 
-        } 
+        };
+
+    toggler.addEventListener('click', function() {
+        toggler.classList.toggle('open')
+        navList.classList.toggle('home__hidList-closed')
+        navigation.classList.toggle('home__hidMenu-close')
+    });
+
     for(var i = 0; i < sliderDots.length; i++) {
         var arr = Array.prototype.slice.call( sliderDots, 0 )
         arr[i].onclick = function() {
@@ -20,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function(){
             slides[currentSlide].className = 'slide showing';
             controlEnb[currentSlide].className = 'control_button controls__enabled';
         }
-    }
+    };
 
     function nextSlide(){
 	    slides[currentSlide].className = 'slide';
